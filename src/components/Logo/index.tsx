609ba317bb8e6d87a96c93e31acd84367ecadc0e
Mateus Prado <maateussp@gmail.com>
2026-01-06 12:03:23 -0300

    build: criação do menu

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,17 @@
+import type { ReactNode } from 'react';
+import styles from './styles.module.css';
+import { TimerIcon } from 'lucide-react';
+
+
+
+export function Logo () {
+
+return <div className={styles.logo}>
+    <a className={styles.logoLink} href="#">
+        <TimerIcon/>
+        <span>Chronos</span>
+        </a>
+    </div>
+
+
+}
