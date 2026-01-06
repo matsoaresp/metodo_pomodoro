caf51bafbfb39695aae2024a8129555566ae2580
Mateus Prado <maateussp@gmail.com>
2026-01-06 09:03:40 -0300

    build: Code to learn props and children

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ export function App() {
 
   return (
     <div>
-      <Heading/>
+      <Heading>Cabeçalho 1</Heading>
+      <Heading>Cabeçalho 2</Heading>
+      <Heading>Cabeçalho 3</Heading>
       <p>Lorem ipsum dolor sit
         amet consectetur adipisicing elit. Consectetur, facere corrupti fugiat ab minus dolorum, quo explicabo et sequi quibusdam nemo eos veritatis magnam nesciunt cumque, enim possimus eligendi accusantium est dolore molestiae quas! Soluta, non officiis eius rerum nesciunt sequi incidu
         nt qui cumque quia vitae. Omnis, distinctio sunt? Dolorem, illo delectus. Quam, nemo exercitationem? Quos impedit, provident expedita ea nisi deserunt tempora, non quia nihil voluptas, excepturi odit enim laboriosam? Cum aliquid aliquam recusandae sunt debitis quibusdam atque molestias ratione quae, possimus facilis itaque iusto ex placeat hic. Blanditiis suscipit quas unde nobis beatae sequi dolorem esse iusto ipsam?</p>
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,8 @@
 import styles from './Heading.module.css';
 
-export function Heading () {
+export function Heading (props) {
 
-return <h1 className={styles.heading}>Titulo</h1>
+return <h1 className={styles.heading}>{props.children}</h1>
 
 
 }
